3ffb7cc6610febdacae6dc106fec106ded324a00
Anas <muhammad.anas30000@gmail.com>
2019-06-13 17:03:03 +0500

    uncommented stuff

--- scraper.php
+++ scraper.php
@@ -3,33 +3,34 @@
     // Create DOM from URL or file
     $html = file_get_html('https://www.youtube.com/feed/trending?gl=GB');
 
-    var_dump($html);
+    //var_dump($html);
     
-//     $videos = [];
+    $videos = [];
 
-//     // Find top ten videos
-//     $i = 1;
-//     foreach ($html->find('ytd-video-renderer.ytd-expanded-shelf-contents-renderer') as $video) {
-//         if ($i > 10) {
-//                 break;
-//         }
+    // Find top ten videos
+    $i = 1;
+    foreach ($html->find('ytd-video-renderer.ytd-expanded-shelf-contents-renderer') as $video) {
+        if ($i > 10) {
+                break;
+        }
 
-//         // Find item link element
-//         $videoDetails = $video->find('a#video-title', 0);
+        // Find item link element
+        $videoDetails = $video->find('a#video-title', 0);
 
-//         // get title attribute
-//         $videoTitle = $videoDetails->title;
+        // get title attribute
+        $videoTitle = $videoDetails->title;
 
-//         // get href attribute
-//         $videoUrl = 'https://youtube.com' . $videoDetails->href;
+        // get href attribute
+        $videoUrl = 'https://youtube.com' . $videoDetails->href;
 
-//         // push to a list of videos
-//         $videos[] = [
-//                 'title' => $videoTitle,
-//                 'url' => $videoUrl
-//         ];
+        // push to a list of videos
+        $videos[] = [
+                'title' => $videoTitle,
+                'url' => $videoUrl
+        ];
 
-//         $i++;
-// }
-// var_dump($videos);
+        $i++;
+}
+var_dump($videos);
+
 ?>
